2eb84534ef483a1600a22fd5f98fafbe58decdb2
lucas-goldner <lucas.goldner@googlemail.com>
2023-05-11 23:28:07 +0200

    refactor: Change structure

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:fluttercon_2023_presentation/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MyPresentation());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
